6dbd03ad9ea03d3216589de4923ee59fa24ceffe
NitschMax <maximilian.nitsch@gmx.de>
2020-11-13 09:12:41 +0100

    Cleaned up version of the project with a README file.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from sandpile import grid
+import numpy as np
+import time
 
 def main():
     N       = 100
-    crit    = .70
+    n       = 100
+    crit    = .9
     lattice = grid(crit, N, N)
-    lattice.fill_random_checker()
     lattice.fill_random()
+    lattice.run(n)
     lattice.plot()
-    for k in range(500):
-        lattice.time_step_mat()
-    lattice.plot()
+    
+
+#    for crit in np.arange(0.78, 0.81, .001):
+#        print(crit)
+#        lattice = grid(crit, N, N)
+#        #lattice.fill_random()
+#        start   = time.perf_counter()
+#        lattice.load()
+#        lattice.run(n)
+#        #lattice.plot()
+#        #print(time.perf_counter() - start)
+#        print()
+#        lattice.save()
 
 
 if __name__ == "__main__":
